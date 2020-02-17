39ce453e10e315d6e2a47b937e885468a076fb53
Francesco Reali <f.reali@outlook.com>
2020-02-17 20:03:55 +0100

    Update render.js

--- public/render.js
+++ public/render.js
@@ -33,7 +33,7 @@ self.addEventListener('fetch', function(event) {
         <meta charset='utf-8'>
         <meta name='viewport' content='width=device-width,initial-scale=1'>
       
-        <title>Svelte ${value}</title>
+        <title>Swsr • ${value.split('').map((x,i) => i == 0 ? x.toUpperCase() : x).join('')}</title>
       
         <link rel='icon' type='image/png' href='/favicon.png'>
         <link rel='stylesheet' href='/global.css'>
@@ -66,61 +66,3 @@ self.addEventListener('fetch', function(event) {
     const value = second.substr(second.indexOf('/'))
     return value
   }
-
-
-
-
-//   var CACHE = 'cache-and-update';
-
-// // On install, cache some resources.
-// self.addEventListener('install', function(evt) {
-//   console.log('The service worker is being installed.');
-
-//   // Ask the service worker to keep installing until the returning promise
-//   // resolves.
-//   evt.waitUntil(precache());
-// });
-
-// // On fetch, use cache but update the entry with the latest contents
-// // from the server.
-// self.addEventListener('fetch', function(evt) {
-//   console.log('The service worker is serving the asset.');
-//   // You can use `respondWith()` to answer immediately, without waiting for the
-//   // network response to reach the service worker...
-//   evt.respondWith(fromCache(evt.request));
-//   // ...and `waitUntil()` to prevent the worker from being killed until the
-//   // cache is updated.
-//   evt.waitUntil(update(evt.request));
-// });
-
-// // Open a cache and use `addAll()` with an array of assets to add all of them
-// // to the cache. Return a promise resolving when all the assets are added.
-// function precache() {
-//   return caches.open(CACHE).then(function (cache) {
-//     return cache.addAll([
-//       './controlled.html',
-//       './asset'
-//     ]);
-//   });
-// }
-
-// // Open the cache where the assets were stored and search for the requested
-// // resource. Notice that in case of no matching, the promise still resolves
-// // but it does with `undefined` as value.
-// function fromCache(request) {
-//   return caches.open(CACHE).then(function (cache) {
-//     return cache.match(request).then(function (matching) {
-//       return matching || Promise.reject('no-match');
-//     });
-//   });
-// }
-
-// // Update consists in opening the cache, performing a network request and
-// // storing the new response data.
-// function update(request) {
-//   return caches.open(CACHE).then(function (cache) {
-//     return fetch(request).then(function (response) {
-//       return cache.put(request, response);
-//     });
-//   });
-// }
